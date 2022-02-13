eeec1f703821e35ce2803cd0db723f5515507867
MertCelik123 <mertcelik2399@gmail.com>
2022-02-13 13:18:34 +0300

    Api ve servis test ornekleri eklendi

---
+++ BaseProject.UnitTest/ApiTest/ArticleControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseProject.API.Controllers;
+using BaseProject.Core.Dtos;
+using BaseProject.Services.Abstract;
+using BaseProject.Shared.Response;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace BaseProject.UnitTest.ApiTest
+{
+    public class ArticleControllerTest
+    {
+        private ArticleController _articleController;
+        private Mock<IArticleService> _articleService;
+
+        public ArticleControllerTest()
+        {
+            _articleService = new Mock<IArticleService>();
+            _articleController = new ArticleController(_articleService.Object);
+        }
+
+        [Fact]
+        public async void GetArticles_OkResult_Success()
+        {
+            List<ArticleDto> articles = new List<ArticleDto>();
+
+            Response<List<ArticleDto>> response = new Response<List<ArticleDto>>(ResultStatus.Success, "test",articles);
+
+            _articleService.Setup(x => x.GetAll()).Returns(Task.FromResult(response));
+
+            var result = await _articleController.GetArticles();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            var returnArticles = Assert.IsType<Response<List<ArticleDto>>>(okResult.Value);
+
+            Assert.Equal(returnArticles.Data, articles);  
+        }
+
+        [Fact]
+        public async void AddArticle_OkResult_Success()
+        {
+            ArticleAddDto article = new ArticleAddDto();
+
+            NoDataResponse response = new NoDataResponse(ResultStatus.Success, "test");
+
+            _articleService.Setup(x => x.Add(article)).Returns(Task.FromResult(response));
+
+            var result = await _articleController.AddArticle(article);
+
+            var okResult = Assert.IsType<OkResult>(result);
+
+            _articleService.Verify(x => x.Add(article), Times.Once);
+        }
+
+
+
+    }
+}
---
+++ BaseProject.UnitTest/ServiceTest/ArticleServiceTest.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using BaseProject.Core;
+using BaseProject.Core.Dtos;
+using BaseProject.Data.Abstract;
+using BaseProject.Services.Abstract;
+using BaseProject.Services.Concrete;
+using BaseProject.Services.Utilities;
+using BaseProject.Shared.Response;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BaseProject.UnitTest.ServiceTest
+{
+    public class ArticleServiceTest
+    {
+        private IArticleService _articleService;
+        private Mock<IUnitOfWork> _unitOfWork;
+        private Mock<IMapper> _mapper;
+        public ArticleServiceTest()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _mapper = new Mock<IMapper>();
+            _articleService = new ArticleService(_unitOfWork.Object, _mapper.Object);
+        }
+
+        [Fact]
+        public async void AddArticle_AddError_WhenDtoIsNull()
+        {
+            ArticleAddDto articleDto = null;
+
+            var result=await _articleService.Add(articleDto);
+
+            Assert.Equal(ResponseMessages.GeneralErrors.AddError(),result.Message);
+            Assert.Equal(ResultStatus.Error,result.ResultStatus);
+
+        }
+
+        [Fact]
+        public async void AddArticle_Succees()
+        {
+            Article article = new Article 
+            {
+                Category=new Category(),
+                CategoryId="1",
+                Content="test",
+                IsActive=true,
+                Title="test",
+                User=new User(),
+                UserId="1"
+            };
+
+            ArticleAddDto articleAddDto = new ArticleAddDto();
+
+            _mapper.Setup(x => x.Map<Article>(articleAddDto)).Returns(article);
+            _unitOfWork.Setup(x => x.Articles.AddAsync(article)).Returns(Task.FromResult(article));
+
+            var result = await _articleService.Add(articleAddDto);
+
+            Assert.Equal(ResponseMessages.Article.Add(article.Title), result.Message);
+            Assert.Equal(ResultStatus.Success, result.ResultStatus);
+
+        }
+
+        [Fact]
+        public async void DeleteArticle_NotFound_WhenArticleIsNull()
+        {
+            string articleId = "1";
+
+            Article article = null;
+
+            _unitOfWork.Setup(x => x.Articles.GetAsync(a=>a.Id==articleId)).Returns(Task.FromResult(article));
+
+            var result = await _articleService.Delete(articleId);
+
+            Assert.Equal(ResponseMessages.Article.NotFound(false), result.Message);
+            Assert.Equal(ResultStatus.Error, result.ResultStatus);
+
+        }
+
+        [Fact]
+        public async void DeleteArticle_Success()
+        {
+            string articleId = "1";
+
+            Article article = new Article() { Title = "test" };
+
+            _unitOfWork.Setup(x => x.Articles.GetAsync(a => a.Id == articleId)).Returns(Task.FromResult(article));
+
+            var result = await _articleService.Delete(articleId);
+
+            Assert.Equal(ResponseMessages.Article.Delete(article.Title), result.Message);
+            Assert.Equal(ResultStatus.Success, result.ResultStatus);
+
+        }
+
+        [Fact]
+        public async void GetArticle_NotFound_WhenArticleIsNull()
+        {
+            string articleId = "1";
+
+            Article article = null;
+
+            _unitOfWork.Setup(x => x.Articles.GetAsync(a => a.Id == articleId)).Returns(Task.FromResult(article));
+
+            var result = await _articleService.Get(articleId);
+
+            Assert.Equal(ResponseMessages.Article.NotFound(false), result.Message);
+            Assert.Equal(ResultStatus.Error, result.ResultStatus);
+
+        }
+
+        [Fact]
+        public async void GetArticle_Success()
+        {
+            string articleId = "1";
+
+            Article article = new Article() { Title = "test" };
+
+            _unitOfWork.Setup(x => x.Articles.GetAsync(a => a.Id == articleId, a => a.User, a => a.Category)).Returns(Task.FromResult(article));
+
+            var result = await _articleService.Get(articleId);
+
+            Assert.Equal(ResultStatus.Success, result.ResultStatus);
+
+        }
+
+
+
+    }
+}
